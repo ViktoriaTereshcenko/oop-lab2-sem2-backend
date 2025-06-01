ced83cca324ccc78cc0a694193dc4cd45d6cee70
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-06-02 01:58:07 +0300

    Create jwt_handler.py

---
+++ ShopV2_backend/app/auth/jwt_handler.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta, timezone
+from jose import JWTError, jwt
+from typing import Optional
+
+SECRET_KEY = "your-secret-key"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def verify_access_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: int = payload.get("sub")
+        if user_id is None:
+            raise JWTError()
+        return user_id
+    except JWTError:
+        return None
