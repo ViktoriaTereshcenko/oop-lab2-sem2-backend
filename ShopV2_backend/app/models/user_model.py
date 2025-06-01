243e60b23faa2c77383b7657277e612284341104
ViktoriaTereshchenko <145542399+ViktoriaTereshcenko@users.noreply.github.com>
2025-06-02 02:03:03 +0300

    Create user_model.py

---
+++ ShopV2_backend/app/models/user_model.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(100), unique=True, nullable=False)
+    password = Column(String(100), nullable=False)
+    role = Column(String(20), nullable=False, default="user")
